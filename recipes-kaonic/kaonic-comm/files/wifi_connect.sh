09b7e35c0a95d52c53180391053a826feac73bdb
n-beechat <nicholasquinn@beechat.network>
2026-02-04 14:58:11 +0000

    Update wifi_connect.sh
    Fixes ssid or password with whitespace. User must use quotes "

--- recipes-kaonic/kaonic-comm/files/wifi_connect.sh
+++ recipes-kaonic/kaonic-comm/files/wifi_connect.sh
@@ -10,7 +10,7 @@ ip link set wlan0 down
 # Set WiFi link Up
 ip link set wlan0 up
 
-wpa_passphrase $1 $2 >> /etc/wpa_supplicant.conf
+wpa_passphrase "$1" "$2" >> /etc/wpa_supplicant.conf
 wpa_supplicant -B -iwlan0 -c /etc/wpa_supplicant.conf
 
 iw wlan0 link
